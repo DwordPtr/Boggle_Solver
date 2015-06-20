3afb8471b1395e9e2f95c375e1c0a492cc96e200
Bryan Tidwell <btidwell4@my.apsu.edu>
2015-06-19 19:35:59 -0500

    really remove boggleSubstrings.py from the source code

--- src/boggleSubstrings.py
+++
@@ -1,6 +0,0 @@
-max_length = 16
-substrings_per_max_length_string = 0
-for i in range(3,max_length+1):
-   substrings_per_max_length_string+=(max_length - i)
-
-print(substrings_per_max_length_string)
